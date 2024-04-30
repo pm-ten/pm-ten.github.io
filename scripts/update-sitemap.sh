72286c219ba23428c9b51a41112e22dfb3062051
Claudio Brotto <claudio@int3.it>
2024-04-30 17:03:43 +0000

    build script typo

--- scripts/update-sitemap.sh
+++ scripts/update-sitemap.sh
@@ -4,7 +4,7 @@ cat $SITEMAP_FILES | while read filename; do
   if [ -n "$filename" ]; then
     # echo "File: $filename."
     token="<lastmod>$filename</lastmod>"
-    lastmod="<lastmod>$(git log -1 --format="%ad" --date=format:'%Y-%m-%dT%H:%M:%SZ'  -- $filename)<lastmod>"
+    lastmod="<lastmod>$(git log -1 --format="%ad" --date=format:'%Y-%m-%dT%H:%M:%SZ'  -- $filename)</lastmod>"
     sed -i "s#$token#$lastmod#" $SITEMAP_OUTPUT
   fi
 done
